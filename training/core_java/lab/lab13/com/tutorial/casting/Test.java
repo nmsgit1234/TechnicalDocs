4213d26eb72266f194d981544a6aab7474e86467
anonymous <anonymous@yahoo.com>
2015-09-04 11:57:59 +0530

    Updated for inheritance

---
+++ training/core_java/lab/lab13/com/tutorial/casting/Test.java
@@ -0,0 +1,9 @@
+package com.tutorial.casting;
+
+public class Test{
+	public static void main(String args[]){
+		int b = 128;
+		byte c = (byte)b;
+		System.out.println("the byte value is " + c);
+	}
+}
